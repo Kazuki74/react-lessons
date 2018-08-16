7e3e63160e09bb87164859081cd245de946fb2f2
小林和希 <kobayashi.kazuki74@gmail.com>
2018-08-16 21:53:42 +0900

    learned jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>
-          Hello, world!
-        </h1>
-      </div>
-    );
+  	// const greeting = "Hi!!";
+  	// CSSのクラスはキャメルケース
+  	// const dom = <h1 className="greet">{greeting}</h1>;
+  	// return dom;
+  	// forとは書けない
+  	// returnするDOMは一つだけ
+  	// divを使いたくないならReact.Fragmentを使う
+  	return (
+
+  		<React.Fragment>
+  			<input type="text" onClick={() => {console.log("clicked!")}}/>
+  			<label htmlFor="bar">Bar</label>
+  		</React.Fragment>
+  	)
   }
 }
 
