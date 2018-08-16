067dfb43dc1311f2c11f22d311a91fadb15d86e2
小林和希 <kobayashi.kazuki74@gmail.com>
2018-08-16 22:38:26 +0900

    created class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-  	// const greeting = "Hi!!";
-  	// CSSのクラスはキャメルケース
-  	// const dom = <h1 className="greet">{greeting}</h1>;
-  	// return dom;
-  	// forとは書けない
-  	// returnするDOMは一つだけ
-  	// divを使いたくないならReact.Fragmentを使う
-  	return (
+// class App extends Component {
+//   render() {
+//   	// const greeting = "Hi!!";
+//   	// CSSのクラスはキャメルケース
+//   	// const dom = <h1 className="greet">{greeting}</h1>;
+//   	// return dom;
+//   	// forとは書けない
+//   	// returnするDOMは一つだけ
+//   	// divを使いたくないならReact.Fragmentを使う
+//   	return (
 
-  		<React.Fragment>
-  			<input type="text" onClick={() => {console.log("clicked!")}}/>
-  			<label htmlFor="bar">Bar</label>
-  		</React.Fragment>
-  	)
-  }
+//   		<React.Fragment>
+//   			<input type="text" onClick={() => {console.log("clicked!")}}/>
+//   			<label htmlFor="bar">Bar</label>
+//   		</React.Fragment>
+//   	)
+//   }
+// }
+
+const App = () => {
+	return (
+		<div>
+			<Cat />
+			<Cat />
+			<Cat />
+			<Cat />
+		</div>
+	)
+}
+
+const Cat = () => {
+	return <div>Meow!</div>
 }
 
 export default App;
