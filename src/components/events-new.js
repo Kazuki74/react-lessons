bbe4824279c1dc34f4bb92a1cecca22b3d8a268d
小林和希 <kobayashi.kazuki74@gmail.com>
2018-08-19 19:49:49 +0900

    implemented post action

--- src/components/events-new.js
+++ src/components/events-new.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 // import { postEvent } from '../actions'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import { Field, reduxForm } from 'redux-form'
+import { postEvents } from '../actions'
 
 class EventsNew extends Component {
-	render() {
+	constructor(props) {
+		super(props)
+		this.onSubmit = this.onSubmit.bind(this)
+	}
+	renderField(field) {
+		const { input, label, type, meta: { touched, error } } = field
 		return (
-			<React.Fragment>
-				<div>Foooo!</div>
-			</React.Fragment>
+			<div>
+				<input {...input} placeholder={label} type= {type} />
+				{touched && error && <span>{ error }</span>}
+			</div>
+		)
+	}
+
+	async onSubmit(values) {
+		await this.props.postEvents(values)
+		this.props.history.push("/")
+	}
+
+	render() {
+		const { handleSubmit } = this.props
+		return (
+			<form onSubmit={ handleSubmit(this.onSubmit) }>
+				<div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+				<div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+				<div>
+					<input type="submit" value="Submit" disabled={false} />
+					<Link to="/">Cancel</Link>
+				</div>
+			</form>
 		)
 	}
 }
 
-// const mapDispatchToProps = ({ postEvents })
+const mapDispatchToProps = ({ postEvents })
 
-// dispatchは以下でもよい
-// const mapDispatchToProps = ({
-// 	increment, decrement
-// })
+const validate = values => {
+	const errors = {}
 
-export default connect(null, null)(EventsNew)
+	if (!values.title) errors.title = "Enter a title!"
+	if (!values.body) errors.body = "Enter a body!"
+
+	return errors
+}
+
+export default connect(null, mapDispatchToProps)(
+	reduxForm({ validate, form:'eventNewForm' })(EventsNew)
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 
 // 下記のように書けば追加もできる
 // export default combineReducers({ foo, bar, baz })
