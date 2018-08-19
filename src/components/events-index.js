b53e9a5b4adb869fa4c084cbd644abeef3d1e9e2
小林和希 <kobayashi.kazuki74@gmail.com>
2018-08-19 16:18:52 +0900

    completed index action

--- src/components/events-index.js
+++ src/components/events-index.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { readEvents } from '../actions'
+import _ from 'lodash'
 
 class EventsIndex extends Component {
 	componentDidMount() {
 		this.props.readEvents()
 	}
 
+	renderEvent() {
+		return _.map(this.props.events, event => (
+			<tr key={event.id}>
+				<td>{event.id}</td>
+				<td>{event.title}</td>
+				<td>{event.body}</td>
+			</tr>
+		))
+	}
+
 	render() {
-		const props = this.props
 		return (
-			<React.Fragment>
-				<div>count: { props.value } </div>
-				<button onClick={props.increment}>+1</button>
-				<button onClick={props.decrement}>-1</button>
-			</React.Fragment>
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Title</th>
+						<th>Body</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.renderEvent()}
+				</tbody>
+			</table>
 		)
 	}
 }
 
-const mapStateToProps = state => ({ })
+const mapStateToProps = state => ({ events: state.events })
 
 const mapDispatchToProps = ({ readEvents })
 
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,21 @@
 import { READ_EVENTS } from '../actions'
+import _ from 'lodash'
 
-export default (state = {}, action) => {
+export default (events = {}, action) => {
 	switch (action.type) {
 		case READ_EVENTS:
-			return state
+		// [
+		// 	{"id":11,"title":"fuga","body":"hoge"},
+		// 	{"id":2,"title":"Let's have an event 2!","body":"This is the body for event 2."}
+		// ]
+		// 上を下のようにしたい(Loadashでオブジェクト化)
+		// console.log(_.mapKeys(action.response.data, 'id'))
+		// [
+		// 	1: {"id":11,"title":"fuga","body":"hoge"},
+		// 	2: {"id":2,"title":"Let's have an event 2!","body":"This is the body for event 2."}
+		// ]
+			return _.mapKeys(action.response.data, 'id')
 		default:
-			return state
+			return events
 	}
 }
